ac002c8562d1de3e6226f7c446381ae70db967f9
Guilherme Húngaro <89612743+GuiHungaro@users.noreply.github.com>
2022-05-12 09:25:57 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,7 @@ templates_path = ['_templates']
 
 html_static_path = ['_static']
 
-html_css_files = [
-    '_static/custom.css',
-]
+html_css_files = ['custom.css', '_static/custom.css']
 
 html_theme = 'sphinx_rtd_theme'
 
